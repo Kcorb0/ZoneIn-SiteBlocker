f8f4dddcdc7441e967815490800b461080e180ff
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2022-06-09 19:42:10 +0100

    Can now add sites while ext is active

--- background.js
+++ background.js
@@ -1,25 +1,19 @@
 chrome.runtime.onInstalled.addListener(() => {
 
     let active = false;
+    let blockedSites = ["twitter.com", "twitch.tv"]
 
     chrome.storage.sync.set({ active });
+    chrome.storage.sync.set({ blockedSites });
+
     console.log(`Extension status: ${active}`);
 });
 
 chrome.tabs.onUpdated.addListener(() => {
-    chrome.storage.sync.get("active", function (data) {
+    chrome.storage.sync.get("active", (data) => {
 
         let status = data.active;
         let redirectUrl = 'https://www.google.co.uk';
-        let siteUrls = [
-            "twitch.tv",
-            "youtube.com",
-            "twitter.com",
-            "reddit.com",
-            "facebook.com",
-            "just-eat.co.uk",
-            "dominos.co.uk"
-        ];
 
         console.log(`Status ${status}`)
 
@@ -27,10 +21,18 @@ chrome.tabs.onUpdated.addListener(() => {
             chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
                 let currentUrl = tabs[0].url;
 
-                siteUrls.forEach(function (item) {
-                    if (currentUrl.includes(item)) {
-                        chrome.tabs.update(undefined, { url: redirectUrl });
-                    }
+                chrome.storage.sync.get("blockedSites", (sitesData) => {
+
+                    let sites = sitesData.blockedSites
+
+                    console.log(sites)
+
+                    sites.forEach((site) => {
+                        if (currentUrl.includes(site)) {
+                            console.log(site)
+                            chrome.tabs.update(undefined, { url: redirectUrl });
+                        }
+                    });
                 });
             });
         }
--- popup.js
+++ popup.js
@@ -2,19 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('activate').addEventListener('click', () => {
         let active = true;
+
         chrome.storage.sync.set({ active });
-        console.log('activated')
     });
 
     document.getElementById('de-activate').addEventListener('click', () => {
         let active = false;
+
         chrome.storage.sync.set({ active });
-        console.log('deactivated')
     });
 
     document.getElementById('refresh').addEventListener('click', () => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.update(undefined, { url: tabs[0].url });
+        });
+    });
+
+    document.getElementById('add-site').addEventListener('click', () => {
+        chrome.storage.sync.get("blockedSites", (sitesData) => {
+            let newSite = document.getElementById('site-inp').value;
+            let blockedSites = sitesData.blockedSites;
+
+            document.getElementById('site-inp').value = "";
+            chrome.storage.sync.set({ "blockedSites": blockedSites.push(newSite) });
         });
     });
 });
